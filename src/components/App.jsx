e09c19b83f8738014e097827ffa3ba3e0a8ccb74
Valeriia Kyrychenko <valeriia.kyrychenko22@gmail.com>
2023-12-04 19:51:58 -0800

    added basic project structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,48 @@
-export const App = () => {
-  return (
-    <div
+// eslint-disable-next-line
+
+import React, { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+
+
+export class App extends React.Component  {
+
+state = {
+  contacts: [],
+  name: ''
+}
+
+
+
+
+  render() {
+   
+    return (
+      
+      <div
       style={{
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+          color: '#010101',
+        flexDirection: 'column'
       }}
     >
-      React homework template
-    </div>
-  );
+        <h1>Phonebook</h1>
+
+        <ContactForm></ContactForm>
+
+        <h2>Contacts</h2>
+
+        <Filter></Filter>
+        <ContactList></ContactList>
+        
+
+      </div>
+    
+    )
+ }
 };
